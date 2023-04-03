9c610fc0c9bccaa10ffec2d094fdf0c7a156fc13
Adamhanafi22 <adamhanafi1922@gmail.com>
2023-04-03 14:37:30 +0700

    membuuat input data

--- activityStructure_129/activityStructure_129.cpp
+++ activityStructure_129/activityStructure_129.cpp
@@ -19,5 +19,17 @@ int main()
 	mhs1.alamat = "lamongan";
 	mhs1.umur = 20;
 
+	cout << "Masukkan nim: ";
+	cin >> mhs2.nim;
+	cout << "Masukkan nama: ";
+	cin >> mhs2.nama;
+	cout << "Masukkan alamat: ";
+	cin >> mhs2.alamat;
+	cout << "Masukkan umur: ";
+	cin >> mhs2.umur;
+
+	
+
 }
+
 
